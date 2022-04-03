0956f830dcec0e394caf223586209e6d4f980081
Kong <congcoi123@gmail.com>
2022-04-03 08:14:19 +0200

    docs: update java-docs by phone

--- src/main/java/com/tenio/core/entity/manager/PlayerManager.java
+++ src/main/java/com/tenio/core/entity/manager/PlayerManager.java
@@ -33,7 +33,7 @@ import com.tenio.core.network.entity.session.Session;
 import java.util.Collection;
 
 /**
- * All supported APIs for player management.
+ * All supported APIs for player management. A management can belong to a room or stand alone.
  */
 public interface PlayerManager extends Manager {
 
@@ -46,19 +46,41 @@ public interface PlayerManager extends Manager {
   void addPlayer(Player player) throws AddedDuplicatedPlayerException;
 
 /**
-* Creates a new player and adds it in to the management list.
+* Creates a new player without session and adds it in to the management list.
 *
 * @param name a unique player's name on the server
 * @return a new instance of {@link Player}
 * @throws AddedDuplicatedPlayerException when a same player is aready available in the management list
 */
+// Changes parameter
   Player createPlayer(String name) throws AddedDuplicatedPlayerException;
 
+/**
+* Creates a new player with session and adds it in to the management list.
+*
+* @param name a unique player's name on the server
+* @param session a session associated to the player
+* @return a new instance of {@link Player}
+* @throws AddedDuplicatedPlayerException when a same player is aready available in the management list
+*/
+// Changes parameter
   Player createPlayerWithSession(String name, Session session)
       throws AddedDuplicatedPlayerException, NullPointerException;
 
+/**
+* Retrieves a player by using its name.
+*
+* @param playerName a unique name of player on the server
+* @return an instance of {@link Player}, other wise <code>null</code>
+*/
   Player getPlayerByName(String playerName);
 
+/**
+* Retrieves a player by using its session.
+*
+* @param session the session associated to player on the server
+* @return an instance of {@link Player}, other wise <code>null</code>
+*/
   Player getPlayerBySession(Session session);
 
   // Retrieves readonly list of players, add iterator
@@ -67,19 +89,63 @@ public interface PlayerManager extends Manager {
   // Retrieves readonly list of sessions, add iterator
   Collection<Session> getAllSessions();
 
+/**
+* Removes a player from the management list.
+*
+* @param playerName the player's name
+* @throws RemovedNonExistentPlayerFromRoomException when the player is unvailable
+*/
   void removePlayerByName(String playerName) throws RemovedNonExistentPlayerFromRoomException;
 
+/**
+* Removes a player from the management list.
+*
+* @param session the session associated to a player
+* @throws RemovedNonExistentPlayerFromRoomException when the player is unvailable
+*/
   void removePlayerBySession(Session session) throws RemovedNonExistentPlayerFromRoomException;
 
+/**
+* Determines whether the management list contains a player by checking its name.
+*
+* @param playerName the player's name
+* @return <code>true</code> if the player is available, otherwise <code>false</code>
+*/
   boolean containsPlayerName(String playerName);
 
+/**
+* Determines whether the management list contains a player by checking its session.
+*
+* @param session the session associated to a player
+* @return <code>true</code> if the player is available, otherwise <code>false</code>
+*/
   boolean containsPlayerSession(Session session);
 
+/**
+* Retrieves a room of the management list.
+*
+* @return an instance of {@link Room} or <code>null</code>
+*/
+// Checks null value
   Room getOwnerRoom();
 
+/**
+* Set a room for the management list.
+*
+* @param an instance of {@link Room} or <code>null</code>
+*/
+// Checks null value
   void setOwnerRoom(Room room);
 
+/**
+* Retrieves the current number of players in the list.
+*
+* @return the current number of players in the list
+*/
   int getPlayerCount();
 
+/**
+* Removes all players from the list.
+*/
   void clear();
 }
--- src/main/java/com/tenio/core/entity/manager/RoomManager.java
+++ src/main/java/com/tenio/core/entity/manager/RoomManager.java
@@ -38,41 +38,129 @@ import java.util.List;
  */
 public interface RoomManager extends Manager {
 
+/**
+* Retrieves the maximum number of rooms on the server.
+*
+* @return the maximum number of room on the server
+*/
   int getMaxRooms();
 
+/**
+* Sets the maximum number of room on the server.
+*
+* @param maxRooms the maximum number of rooms
+*/
   void setMaxRooms(int maxRooms);
 
+/**
+* Adds a new room to the server.
+*
+* @param room an instance of {@link Room}
+* @throws AddedDuplicatedRoomException when a room is already available on the server
+*/
   void addRoom(Room room) throws AddedDuplicatedRoomException;
 
+/**
+* Creates a new room without owner and adds it to the server.
+*
+* @param roomSetting all settings created by a {@link InitialRoomSetting} builder
+* @return an instance of {@link Room}
+* @throws IllegalArgumentException when an invalid setting builder set
+*/
   default Room createRoom(InitialRoomSetting roomSetting)
       throws IllegalArgumentException, CreatedRoomException {
     return createRoomWithOwner(roomSetting, null);
   }
 
+/**
+* Creates a new room with owner and adds it to the server.
+*
+* @param roomSetting all settings created by a {@link InitialRoomSetting} builder
+* @param player a {@link Player} as the room's owner
+* @return an instance of {@link Room}
+* @throws IllegalArgumentException when an invalid setting builder set
+* @throws CreatedRoomException when it fails to create a new room
+*/
   Room createRoomWithOwner(InitialRoomSetting roomSetting, Player player)
       throws IllegalArgumentException, CreatedRoomException;
 
+/**
+* Determines whether room is in the management list by looking for its unique Id.
+*
+* @param roomId the <code>long</code> value room's id
+* @return <code>true</code> if the searching room is available, otherwise <code>false</code>
+*/
   boolean containsRoomId(long roomId);
 
+/**
+* Determines whether room is in the management list by looking for its name.
+*
+* @param roomName the {@link String} value room's name
+* @return <code>true</code> if the searching room is available, otherwise <code>false</code>
+*/
   boolean containsRoomName(String roomName);
 
+/**
+* Retrieves a room instance by looking for its unique Id.
+*
+* @param roomId the <code>long</code> value room's id
+* @return an instance of {@link Room} if the searching room is available, otherwise <code>null</code>
+*/
   Room getRoomById(long roomId);
 
+// changes to use iterator (consider to remove)
   List<Room> getRoomListByName(String roomName);
 
+// changes to use iterator (consider to remove)
   Collection<Room> getRoomList();
+  
+  // get a interior unchangable list of room
 
   void removeRoomById(long roomId);
 
+/**
+* Updates a room's name.
+*
+* @param room the updating {@link Room}
+* @param roomName new {@link String} value of room's name
+* @throws IllegalArgumentException when invalid name is set
+*/
+// Check strategy
   void changeRoomName(Room room, String roomName) throws IllegalArgumentException;
 
+/**
+* Updates a room's password.
+*
+* @param room the updating {@link Room}
+* @param roomPassword new {@link String} value of room's password
+* @throws IllegalArgumentException when invalid password is set
+*/
+// Check strategy
   void changeRoomPassword(Room room, String roomPassword) throws IllegalArgumentException;
 
+/**
+* Updates a room's capacity.
+*
+* @param room the updating {@link Room}
+* @param maxPlayers new <code>integer</code> maximum number of players allows in the room
+* @param maxSpectators new <code>integer</code> maximum number of spectators allows in the room
+* @throws IllegalArgumentException when invalid value is set
+*/
   void changeRoomCapacity(Room room, int maxPlayers, int maxSpectators)
       throws IllegalArgumentException;
 
+/**
+* Fetches the current number of rooms in the management list.
+*
+* @return the current number of rooms
+*/
   int getRoomCount();
 
+/**
+* Removes all rooms in the management list.
+*
+* @throws UnsupportedOperationException the operation is not supported at the moment
+*/
   default void clear() {
     throw new UnsupportedOperationException();
   }
