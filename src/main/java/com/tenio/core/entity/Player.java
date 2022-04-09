65e79b03bd6439db8c3b5b3bce0ca305191858d5
Kong <congcoi123@gmail.com>
2022-04-09 07:13:31 +0200

    docs: update java-docs by phone

--- src/main/java/com/tenio/core/entity/Player.java
+++ src/main/java/com/tenio/core/entity/Player.java
@@ -27,64 +27,176 @@ package com.tenio.core.entity;
 import com.tenio.core.network.entity.session.Session;
 
 /**
- * The abstract player object used in the server.
+ * The abstract player entity used on the server.
  */
 public interface Player {
 
   /**
-   * Retrieve the player's name. This value must be unique.
+   * Retrieves the player's name. This value must be unique.
    *
    * @return the player's name in (@link String}
    */
   String getName();
 
+/**
+* Determines whether the player contains a session.
+*
+* @return <code>true</code> if the player contains a session, otherwise <code>false</code>
+*/
   boolean containsSession();
 
+/**
+* Determines whether the player is in a state.
+*
+* @param state a {@link PlayerState} is in judgment
+* @return <code>true</code> if the player in that state, otherwise <code>false</code>
+*/
   boolean isState(PlayerState state);
 
+/**
+* Retrieves the player's current state.
+*
+* @return the current {@link PlayerState}
+*/
   PlayerState getState();
 
+/**
+* Sets current state value for the player.
+*
+* @param state a new value of {@link PlayerState} set to the player
+*/
   void setState(PlayerState state);
 
+/**
+* Determines whether the player is activated.
+*
+* @return <code>true</code> if the player is activated, otherwise <code>false</code>
+*/
   boolean isActivated();
 
+/**
+* Sets the current active state for the player.
+*
+* @param activated the new <code>boolean</code> state of player
+*/
   void setActivated(boolean activated);
 
+/**
+* Determines whether the player is loggedin the server.
+*
+* @return <code>true</code> if the player is loggedin, otherwise <code>false</code>
+*/
   boolean isLoggedIn();
 
+/**
+* Sets the current loggedin state for the player.
+*
+* @param loggedIn the new <code>boolean</code> state of player
+*/
   void setLoggedIn(boolean loggedIn);
 
+/**
+* Retrieves the last loggedin time in miliseconds of the player on the server.
+*
+* @return a <code>long</code> miliseconds value
+*/
   long getLastLoggedInTime();
 
+// Use Optional
   Session getSession();
 
+/**
+* Associates a session to the player.
+*
+* @param session a {@link Session}
+*/
   void setSession(Session session);
 
+/**
+* Determines whether the player is in a room.
+*
+* @return <code>true</code> if the player is in a room, otherwise <code>false</code>
+*/
   boolean isInRoom();
 
+/**
+* Determines whether the player plays as a spectator in a room.
+*
+* @return <code>true</code> if the player is playing as a spectator, otherwise <code>false</code>
+*/
   boolean isSpectator();
 
+/**
+* Marks the player working as a spectator.
+*
+* @param isSpectator set value to <code>true</code> for marking the player as a spectator, contrary set it to <code>false</code>
+*/
   void setSpectator(boolean isSpectator);
 
+// Use Optional
   Room getCurrentRoom();
 
+/**
+* Sets associated room to the player.
+*
+* @param room an instance of {@link Room}
+*/
   void setCurrentRoom(Room room);
 
+/**
+* Retrieves the last time the player left its room.
+*
+* @return the miliseconds in <code>long</code> value
+*/
   long getLastJoinedRoomTime();
 
+/**
+* Retrieves the player's slot in its room.
+*
+* @return the <code>integer</code> value of the player slot position
+*/
   int getPlayerSlotInCurrentRoom();
 
+/**
+* Set a slot value for the player in its room.
+*
+* @param slot the <code>integer</code> value of the player slot position
+*/
   void setPlayerSlotInCurrentRoom(int slot);
 
+// Use Optional
   Object getProperty(String key);
 
+/**
+* Sets a property belongs to the player.
+*
+* @param key the {@link String} key
+* @param value an instance {@link Object} of property's value
+*/
   void setProperty(String key, Object value);
-
+  
+  /**
+  * Determines whether a property is available for the player.
+  *
+  * @param key a {@link String} key used for searching the corresponding property
+  * @return <code>true</code> if the property is available, otherwise <code>false</code>
+  */
   boolean containsProperty(String key);
 
+/**
+* Removes a property which belongs to the player.
+*
+* @param key the {@link String} key
+*/
   void removeProperty(String key);
 
+/**
+* Removes all properties of the player.
+*/
   void clearProperties();
 
+/**
+* Wipes out all the player's information.
+*/
   void clean();
 }
--- src/main/java/com/tenio/core/entity/Room.java
+++ src/main/java/com/tenio/core/entity/Room.java
@@ -38,118 +38,357 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * The abstract room object used in the server.
+ * The abstract room object used on the server.
  */
 public interface Room {
 
+/**
+* Retrieves the room's Id.
+*
+* @return the room's Id in <code>long</code> value
+*/
   long getId();
 
+/**
+* Retrieves the room's name.
+*
+* @return the {@link String} value for room's name
+*/
   String getName();
 
+/**
+* Sets the room's name.
+*
+* @param name a {@link String} value for the room's name
+* @throws IllegalArgumentException when an invalid value is used
+*/
   void setName(String name) throws IllegalArgumentException;
 
+/**
+* Retrieves the room's password.
+*
+* @return the {@link String} value for room's password
+*/
   String getPassword();
 
+/**
+* Sets the room's password.
+*
+* @param password a {@link String} value for the room's password
+* @throws IllegalArgumentException when an invalid value is used
+*/
   void setPassword(String password) throws IllegalArgumentException;
 
+/**
+* Retrieves the current room's state.
+*
+* @return the current {@link RoomState}
+*/
   RoomState getState();
 
+/**
+* Sets new state for the room.
+*
+* @param state the new {@link RoomState} value
+*/
   void setState(RoomState state);
 
+/**
+* Determines whether the room is public which means it does not contain credential and allow to be free to get in.
+*
+* @return <code>true</code> if the room is public, otherwise <code>false</code>
+*/
   boolean isPublic();
 
+/**
+* Retrieves the maximum number of players allows in the room.
+*
+* @return the maximum number of players in <code>integer</code> value
+*/
   int getMaxPlayers();
 
+/**
+* Sets the maximum number of players allows in the room.
+*
+* @param maxPlayers the maximum number of players in <code>integer</code> value
+* @throws IllegalArgumentException when an invalid value is used
+*/
   void setMaxPlayers(int maxPlayers) throws IllegalArgumentException;
 
+/**
+* Retrieves the maximum number of spectators allows in the room.
+*
+* @return the maximum number of spectators in <code>integer</code> value
+*/
   int getMaxSpectators();
 
+/**
+* Sets the maximum number of spectators allows in the room.
+*
+* @param maxSpectators the maximum number of spectators in <code>integer</code> value
+* @throws IllegalArgumentException when an invalid value is used
+*/
   void setMaxSpectators(int maxSpectators) throws IllegalArgumentException;
 
+/**
+*
+*
+*
+*/
+// Use Optional
   Player getOwner();
 
+/**
+* Updates the room's owner.
+*
+* @param owner a {@link Player} is set to be the room's owner
+*/
   void setOwner(Player owner);
 
+/**
+* Retrieves a player manager for the room.
+*
+* @return a {@link PlayerManager} instance
+*/
   PlayerManager getPlayerManager();
 
+/**
+* Sets a new player manager for the room.
+*
+* @param playerManager the {@link PlayerManager} object
+*/
   void setPlayerManager(PlayerManager playerManager);
 
+/**
+* Determines whether the room is activated.
+*
+* @return <code>true</code> when the room is active, otherwise <code>false</code>
+*/
   boolean isActivated();
 
+/**
+* Updates the active state for the room.
+*
+* @param activated sets the value to <code>true</code> to activate the room, otherwise <code>false</code>
+*/
   void setActivated(boolean activated);
 
+/**
+* Retrieves the rule applied to room to remove it.
+*
+* @return the current {@link RoomRemoveMode} of room
+*/
   RoomRemoveMode getRoomRemoveMode();
 
+/**
+* Sets the rule applied to room to remove it.
+*
+* @param roomRemoveMode the new {@link RoomRemoveMode} of room
+*/
   void setRoomRemoveMode(RoomRemoveMode roomRemoveMode);
 
+  // Use Optional
   Object getProperty(String key);
 
+/**
+* Sets a property belongs to the player.
+*
+* @param key the {@link String} key
+* @param value an instance {@link Object} of property's value
+*/
+  void setProperty(String key, Object value);
+  
+  /**
+  * Determines whether a property is available for the player.
+  *
+  * @param key a {@link String} key used for searching the corresponding property
+  * @return <code>true</code> if the property is available, otherwise <code>false</code>
+  */
   boolean containsProperty(String key);
 
-  void addProperty(String key, Object value);
-
+/**
+* Removes a property which belongs to the player.
+*
+* @param key the {@link String} key
+*/
   void removeProperty(String key);
 
+/**
+* Removes all properties of the player.
+*/
+  void clearProperties();
+
+/**
+* Retrieves the total number of entities allowed to be in the room (players and spectators)
+*
+* @return the <code>integer</code> value, total number of entities
+*/
   int getCapacity();
 
+/**
+* Sets the limitation values for players and spectators.
+*
+* @param maxPlayers the maximum number of players in <code>integer</code> value
+* @param maxSpectators the maximum number of spectators in <code>integer</code> value
+* @throws IllegalArgumentException when an invalid value is used
+*/
   void setCapacity(int maxPlayers, int maxSpectators) throws IllegalArgumentException;
 
+// iterator, unmodify list
   List<Player> getPlayersList();
 
+// iterator, unmodify list
   List<Player> getSpectatorsList();
 
+/**
+* Retrieves the current number of players in the room.
+*
+* @return the <code>integer</code> value, the current number of players
+*/
   int getPlayerCount();
 
+/**
+* Retrieves the current number of spectators in the room.
+*
+* @return the <code>integer</code> value, the current number of spectators
+*/
   int getSpectatorCount();
 
+/**
+* Determines whether a player is present in the room.
+*
+* @param playerName a unique {@link String} player name in the room's player management list
+* @return <code>true</code> when a player can be found, otherwise <code>false</code>
+*/
   boolean containsPlayerName(String playerName);
 
+// Use Optional
   Player getPlayerByName(String playerName);
 
+// Use Optional
   Player getPlayerBySession(Session session);
 
+// iterator, unmodify list
   Collection<Player> getAllPlayersList();
 
+// iterator, unmodify list
   Collection<Session> getAllSessionList();
 
+/**
+* Adds a player to the room.
+*
+* @param player a joining {@link Player}
+* @param asSpectator the player join room as a spectator
+* @param targetSlot when the player join the room as role of 'player', then it can occupy a <code>integer</code> slot position
+* @throws PlayerJoinedRoomException any exception occured when the player try to join the room
+* @throws AddedDuplicatedPlayerException when the player has been already in the room, but it is forced to join again
+*/
   void addPlayer(Player player, boolean asSpectator, int targetSlot)
       throws PlayerJoinedRoomException, AddedDuplicatedPlayerException;
 
+/**
+* Adds a player to the room without indicating any slot position.
+*
+* @param player a joining {@link Player}
+* @param asSpectator the player join room as a spectator
+* @throws PlayerJoinedRoomException any exception occured when the player try to join the room
+* @throws AddedDuplicatedPlayerException when the player has been already in the room, but it is forced to join again
+*/
   default void addPlayer(Player player, boolean asSpectator)
       throws PlayerJoinedRoomException, AddedDuplicatedPlayerException {
     addPlayer(player, asSpectator, RoomImpl.DEFAULT_SLOT);
   }
 
+/**
+* Adds a player to the room without indicating any slot position and it join as role of a 'player'.
+*
+* @param player a joining {@link Player}
+* @throws PlayerJoinedRoomException any exception occured when the player try to join the room
+* @throws AddedDuplicatedPlayerException when the player has been already in the room, but it is forced to join again
+*/
   default void addPlayer(Player player)
       throws PlayerJoinedRoomException, AddedDuplicatedPlayerException {
     addPlayer(player, false);
   }
 
+/**
+* Removes a player from its current room.
+*
+* @param player the leaving {@link player}
+* @throws RemovedNonExistentPlayerFromRoomException when the player has already left the room but it is mentioned again
+*/
   void removePlayer(Player player) throws RemovedNonExistentPlayerFromRoomException;
 
+/**
+* Changes a player's role from 'player' to 'spectator' in the room.
+*
+* @param player the changing {@link Player}
+* @throws SwitchedPlayerSpectatorException when the changing cannot be done
+*/
   void switchPlayerToSpectator(Player player) throws SwitchedPlayerSpectatorException;
 
+/**
+* Changes a player's role from 'spectator' to 'player' in the room.
+*
+* @param player the changing {@link Player}
+* @param targetSlot a new <code>integer</code> value of slot position set to the player in the room
+* @throws SwitchedPlayerSpectatorException when the changing cannot be done
+*/
   void switchSpectatorToPlayer(Player player, int targetSlot)
       throws SwitchedPlayerSpectatorException;
 
+/**
+* Changes a player's role from 'spectator' to 'player' without consideration of its slot position in the room.
+*
+* @param player the changing {@link Player}
+* @throws SwitchedPlayerSpectatorException when the changing cannot be done
+* @see RoomImpl#DEFAULT_SLOT
+*/
   default void switchSpectatorToPlayer(Player player) throws SwitchedPlayerSpectatorException {
     switchSpectatorToPlayer(player, RoomImpl.DEFAULT_SLOT);
   }
 
+/**
+* Determines whether the room is empty.
+*
+* @return <code>true</code> if there is no entity in the room, otherwise <code>false</code>
+*/
   boolean isEmpty();
 
+/**
+* Determines whether the room is full.
+*
+* @return <code>true</code> if the number of entities reached the limitation in the room, otherwise <code>false</code>
+*/
   boolean isFull();
 
+// Use Optional
   RoomPlayerSlotGeneratedStrategy getPlayerSlotGeneratedStrategy();
 
+/**
+* Sets a strategy to generate slot positions in the room.
+*
+* @param roomPlayerSlotGeneratedStrategy an implemented of {@link RoomPlayerSlotGeneratedStrategy}
+*/
   void setPlayerSlotGeneratedStrategy(
       RoomPlayerSlotGeneratedStrategy roomPlayerSlotGeneratedStrategy);
 
+// Use Optional
   RoomCredentialValidatedStrategy getRoomCredentialValidatedStrategy();
 
+/**
+* Sets a strategy to validate credentials used to allow players get in the room.
+*
+* @param roomCredentialValidatedStrategy an implemented of {@link RoomCredentialValidatedStrategy}
+*/
   void setRoomCredentialValidatedStrategy(
       RoomCredentialValidatedStrategy roomCredentialValidatedStrategy);
 
+/**
+* Clears all information about the room.
+*
+* @throws UnsupportedOperationException the operation is not supported at the moment
+*/
   default void clear() {
     throw new UnsupportedOperationException();
   }
