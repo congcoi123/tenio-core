421a60f1b1035be5f08c764c9d7aa0b888ceafc5
Kong <congcoi123@gmail.com>
2022-04-13 08:25:19 +0200

    docs: update java-docs by phone

--- src/main/java/com/tenio/core/network/entity/protocol/Request.java
+++ src/main/java/com/tenio/core/network/entity/protocol/Request.java
@@ -33,23 +33,77 @@ import com.tenio.core.network.entity.session.Session;
  */
 public interface Request {
 
+/**
+* Retrieves the unique Id of request.
+*
+* @return the unique <code>long</code> Id of request
+*/
   long getId();
 
+/**
+* Retrieves an additional information of the request.
+*
+* @param key the {@link String} value key used to fetch information
+* @return the corresponding {@link Object} value if available, otherwise <code>null</code>
+*/
   Object getAttribute(String key);
 
+/**
+* Puts an additional information for the request.
+*
+* @param key the {@link String} value key used to fetch information
+* @param value the {@link Object} value
+* @return the pointer of request
+*/
   Request setAttribute(String key, Object value);
 
+/**
+* Retrieves the server event associating to the request.
+*
+* @return a {@link ServerEvent} associating to the request
+*/
   ServerEvent getEvent();
 
+/**
+* Sets a server event associating to the request.
+*
+* @param event a {@link ServerEvent} associating to the request
+* @return the pointer of request
+*/
   Request setEvent(ServerEvent event);
 
+/**
+* Retrieves the sender of request.
+*
+* @return a {@link Session} that plays as a sender of the request
+*/
   Session getSender();
 
+/**
+* Sets the sender of request.
+*
+* @param session a {@link Session} that plays as a sender of the request
+* @return the pointer of request
+*/
   Request setSender(Session session);
 
+/**
+* Retrieves the priority of request.
+*
+* @return {@link RequestPriority} priority of request
+* @see RequestComparator
+*/
   RequestPriority getPriority();
 
+/**
+* Sets priority for the request.
+*
+* @param priority {@link RequestPriority} priority of request
+* @return the pointer of request
+* @see RequestComparator
+*/
   Request setPriority(RequestPriority priority);
 
+// Changes to the created timestamp
   long getTimestamp();
 }
