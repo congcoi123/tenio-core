3249065875f752b53af13f905dc3740ef89840e8
Kong <congcoi123@gmail.com>
2026-04-09 20:09:57 +0200

    chore(CR-47): minor changes

--- src/main/java/com/tenio/core/scheduler/SchedulerImpl.java
+++ src/main/java/com/tenio/core/scheduler/SchedulerImpl.java
@@ -101,7 +101,7 @@ public final class SchedulerImpl extends AbstractManager implements Scheduler {
   @Override
   public void start() {
     if (isInfoEnabled()) {
-      info("START SERVICE", buildgen(getName(), " (", 1, ")"));
+      info("START SCHEDULER", buildgen(getName(), " (", 1, ")"));
     }
 
     taskManager.create("auto-disconnect-player", autoDisconnectPlayerTask);
@@ -141,7 +141,7 @@ public final class SchedulerImpl extends AbstractManager implements Scheduler {
   private void attemptToShutdown() {
     taskManager.clear();
     if (isInfoEnabled()) {
-      info("STOPPED SERVICE", buildgen(getName(), " (", 1, ")"));
+      info("STOPPED SCHEDULER", buildgen(getName(), " (", 1, ")"));
     }
   }
 
